49f32b36eda19a2dda0bbd461084fb7c31250070
Nesaco <126518153+Nesaco@users.noreply.github.com>
2023-03-02 11:53:33 -0500

    nesaco
    correccion de ejercicio

--- piramidImpar.py
+++ piramidImpar.py
@@ -1,5 +1,11 @@
-numero=int(input('ingrese numero: '))
+numero=int(input('ingrese numero: ')) #9
 impar=int(1)
-for i in range(1,int(numero/2)+1):
-    print(str(impar) + ' ')
-impar=impar+2
+numcol=1
+while numcol<numero+1:
+  fila=impar
+  pintarFila=' '
+  while fila<=int(numcol):
+    pintarFila=pintarFila+str(fila)
+    fila=fila+2
+  print(pintarFila)
+  numcol=numcol+2
