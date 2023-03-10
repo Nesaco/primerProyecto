226c18913dfff9bae6564465b62f316b7ebcc775
Nesaco <126518153+Nesaco@users.noreply.github.com>
2023-03-10 12:39:13 -0500

    nesaco

---
+++ Mokemon/js/mokepon.js
@@ -0,0 +1,5 @@
+function seleccionarPersonaje() {
+    alert('SELECCIONAR TU PERSONAJE')
+}
+let botonPersonaje=document.getElementById('boton-personaje')
+botonPersonaje.addEventListener('click', seleccionarPersonaje)
