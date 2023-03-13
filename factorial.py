79c6040b2efe2983744444c88d07a7bc5c820446
Nesaco <126518153+Nesaco@users.noreply.github.com>
2023-03-13 11:12:50 -0500

    listo

---
+++ factorial.py
@@ -0,0 +1,12 @@
+
+def factorial(n):
+  
+  if n==0 or n==1:
+   resultado=1
+  elif n>1:
+    
+    resultado=n*factorial(n-1)
+  return resultado
+
+numero=factorial(int(input('ingrese el numero: ')))
+print(numero)
