07de8d334ccfa64535117db6f4d676b8d723850c
Nesaco <126518153+Nesaco@users.noreply.github.com>
2023-03-14 17:14:30 -0500

    mejoras

--- alumnos.py
+++ alumnos.py
@@ -1,5 +1,3 @@
-#Realizar un programa que conste de una clase llamada Alumno que tenga como atributos el nombre y la nota del alumno. Definir los métodos para inicializar sus atributos, imprimirlos y mostrar un mensaje con el resultado de la nota y si ha aprobado o no.
-
 
 class alumno:
   def atributo(self,nota,alumno):
