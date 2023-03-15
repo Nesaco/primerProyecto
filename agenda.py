d6586a671d40c15a2d7d82f8e81d374f115907df
Nesaco <126518153+Nesaco@users.noreply.github.com>
2023-03-15 15:01:10 -0500

    nesaco
    agenda utilizando class en python

--- agenda.py
+++ agenda.py
@@ -1,32 +1,39 @@
-#Realizar una clase que administre una agenda. Se debe almacenar para cada contacto el nombre, el teléfono y el email. Además deberá mostrar un menú con las siguientes opciones
-#Añadir contacto
-#Lista de contactos
-#Buscar contacto
-#Editar contacto
-#Cerrar agenda
+# Realizar una clase que administre una agenda. Se debe almacenar para cada contacto el nombre, el teléfono y el email. Además deberá mostrar un menú con las siguientes opciones
+# Añadir contacto
+# Lista de contactos
+# Buscar contacto
+# Editar contacto
+# Cerrar agenda
+class contacto:
+    def __init__(self, nombre, telefono, email):
+        self.nombre = nombre
+        self.telefono = telefono
+        self.email = email
+
+    def __str__(self):
+        return f"nombre: {self.nombre}, telefono: {self.telefono}, email: {self.email}. "
 
 class agenda:
-  lista=[]
-def contacto(self,nombre='',telefono=0,email=''):
-      lista=contacto(nombre,telefono,email)
-      self.lista.append(lista)
-      print(f"El contacto es: {lista} !!GUARDADO¡¡")
+    
+    _lista = {'juan':contacto('juan',7879491362, 'hrvhriumxc')}
+    def crear_contacto(self, nombre='', telefono=0, email=''):
+        self._lista[nombre]=contacto(nombre, telefono, email)
+    
+    def mostrar_contactos(self):
+        for contacto in self._lista:
+            print(self._lista.get(contacto))
 
-class contacto:
-  def __init__(self, nombre,telefono,email):
-      self.nombre=nombre
-      self.telefono=telefono
-      self.email=email
-  def __str__(self):
-      return f": {self.nombre},{self.telefono},{self.email} "
+    def buscar_contacto(self,nombre):
+            print(self._lista.get(nombre))
+    
+    
 
-menu=0
+menu = 0
 
-contactos=agenda()
+contactos = agenda()
 
-
-while menu!=5:
-    menu=int(input('''Que desea realizar:
+while menu != 5:
+    menu = int(input('''Que desea realizar:
 1.Añadir contacto
 2.Ver lista de contactos
 3.Buscar contacto
@@ -34,8 +41,17 @@ while menu!=5:
 5.Cerrar agenda
 '''))
 
-    if menu==1:
-        contactos.contacto(input('ingrese nombre: '),input('ingrese telefono: '),input('ingrese email: '))
+    if menu == 1 or menu == 4:
+        contactos.crear_contacto(input('ingrese nombre: '), input(
+            'ingrese telefono: '), input('ingrese email: '))
+
+    elif menu == 2:
+        contactos.mostrar_contactos()
+
+
+    elif menu==3:
+        contactos.buscar_contacto(input('Que nombre desea buscar: '))
     
-    elif menu==2:
-        print(contactos.lista)
+    
+    
+   
