64d525c6c98b6bf9a6c9c0e52647306eea11cf55
Nesaco <126518153+Nesaco@users.noreply.github.com>
2023-03-16 10:37:21 -0500

    Escribir una clase en python llamada rectangulo que contenga una base y una altura, y que contenga un método que devuelva el área del rectángulo.

---
+++ triangulo.py
@@ -0,0 +1,12 @@
+#Escribir una clase en python llamada rectangulo que contenga una base y una altura, y que contenga un método que devuelva el área del rectángulo.
+
+class triangulito:
+    
+    def area(self,base,altura):
+        self.base=base
+        self.altura=altura
+        return(f"El area del tringulo es: {(self.base*self.altura)/2}")
+triangulote=triangulito()
+print(triangulote.area(178,678))
+    
+
