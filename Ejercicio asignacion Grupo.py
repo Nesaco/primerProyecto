f174f435ca58f540e42ccf0aa71ebcd3fbb3addc
Nesaco <126518153+Nesaco@users.noreply.github.com>
2023-03-17 14:24:04 -0500

    .

--- Ejercicio asignacion Grupo.py
+++ Ejercicio asignacion Grupo.py
@@ -14,9 +14,9 @@ dato=alumno()
 
 while continuar=='si':
     dato=input('Ingrese los datos del alumno: ')
-    if dato.estudiante==(self.sexo('femenino')):
+    if dato.sexo=='mujer':
         GrupoA.append(alumno)
-    elif dato.estudiante=='masculino':
+    elif dato.estudiante=='hombre':
         GrupoB.append(alumno)
     print(GrupoA)
     print(GrupoB)
